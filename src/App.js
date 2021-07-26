c03b9863e4f0806cc7fff4b286846ca5955b698d
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-07-26 12:17:05 +0700

    data fetching in react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 function App() {
+  const [emojisData, setEmojisData] = useState([]);
+
+  useEffect(() => {
+    async function fetchEmojis() {
+      const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28');
+
+      console.log(res);
+    }
+
+    fetchEmojis();
+  }, [])
+
   return (
     <div>
       <h1>Hello, world!</h1>
