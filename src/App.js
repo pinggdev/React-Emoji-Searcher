1f6773151c1e4161548a09a53f931c722f73735f
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-07-27 10:37:46 +0700

    dynamic rendering based on fetching status

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
   const [emojisData, setEmojisData] = useState([]);
@@ -11,7 +11,9 @@ function App() {
       setLoading(true);
 
       try {
-        const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28');
+        const res = await axios.get(
+          "https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28"
+        );
 
         setEmojisData(res.data);
         setLoading(false);
@@ -24,15 +26,15 @@ function App() {
     }
 
     fetchEmojis();
-  }, [])
-
-  console.log('loading ? ', loading);
-  console.log('error ? ', error);
-  console.log('emojisData ? ', emojisData);
+  }, []);
 
   return (
     <div>
       <h1>Hello, world!</h1>
+
+      {loading && <p>Loading....</p>}
+      {error && <p>Ooopsss....</p>}
+      {emojisData.length > 0 && <p>Emojis Data sukses!</p>}
     </div>
   );
 }
