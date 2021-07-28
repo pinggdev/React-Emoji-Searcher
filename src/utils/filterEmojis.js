b8fe5bdc963d461d76d5faa8672dbd68ebb0a360
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-07-28 23:54:07 +0700

    create filterEmojis Utils

---
+++ src/utils/filterEmojis.js
@@ -0,0 +1,19 @@
+export const filterEmojis = ({
+  emojisData,
+  searchText = "",
+  maxResult = 20,
+}) => {
+  const filteredEmojis = emojisData.filter((emoji) => {
+    if (emoji.title.toLowerCase().includes(searchText.toLowerCase())) {
+      return true;
+    }
+
+    if (emoji.keywords.toLowerCase().includes(searchText.toLowerCase())) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return filteredEmojis.splice(0, 20);
+};
