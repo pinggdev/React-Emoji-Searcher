f7fdbe31c5718b49f583dda5b2544fa127b8bded
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-07-29 15:27:08 +0700

    create handleSearchEmojis onChange function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Emojis from "./components/Emojis";
 
 function App() {
   const [emojisData, setEmojisData] = useState([]);
+  const [searchText, setSearchText] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -34,19 +35,22 @@ function App() {
     fetchEmojis();
   }, []);
 
+  const handleSearchEmojis = (event) => {
+    setSearchText(event.target.value);
+  };
+
   return (
     <>
       <Navbar />
 
       <Container>
-        <Input 
-          onChange={}
-          value={}
-        />
+        <Input onChange={handleSearchEmojis} value={searchText} />
 
         {loading && <Empty text="Loading..." />}
         {error && <Empty text="Error!" />}
-        {emojisData.length > 0 && <Emojis emojisData={emojisData} />}
+        {emojisData.length > 0 && (
+          <Emojis emojisData={emojisData} searchText={searchText} />
+        )}
       </Container>
     </>
   );
